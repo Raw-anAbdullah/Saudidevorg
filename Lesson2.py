ec7fba9d583f932188dcf64215e97f781c1a1b59
Raw-anAbdullah <ran.alsleebi@gmail.com>
2019-08-21 22:11:36 +0300

    Lesson 2 Uploaded

---
+++ Lesson2.py
@@ -0,0 +1,12 @@
+print("Hello World")
+
+if 5 > 2:
+    print("Five is greater the Two!")
+
+"""
+Utlizing multiline comments
+...
+Code below  will show an error (Code is from lesson 1)
+if 5 > 2:
+print("Five is greater the Two!")
+"""
